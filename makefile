b06efdb71fbd09fab5b047a9c366a0db27b55912
Faith Lovell <faithlxvell@gmail.com>
2023-10-31 13:44:41 -0400

    omp and makefile fix

--- makefile
+++ makefile
@@ -3,8 +3,8 @@ image:image.c image.h
 	gcc -g image.c -o image -lm
 pthreadimage:pthreadimage.c image.h
 	gcc -g pthreadimage.c -o pthreadimage -lm -lpthread
-openmpimage: ompimage.c image.h
-	gcc -g -fopenmp ompimage.c -o ompimage -lm
+ompimage: ompimage.c image.h
+	gcc -g ompimage.c -o ompimage -lm -fopenmp
 
 clean:
-	rm -f image output.png
+	rm -f image pthreadimage ompimage output.png
--- ompimage.c
+++ ompimage.c
@@ -5,7 +5,6 @@
 #include "image.h"
 #include <omp.h>
 
-
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
@@ -61,7 +60,8 @@ uint8_t getPixelValue(Image* srcImage,int x,int y,int bit,Matrix algorithm){
 void convolute(Image* srcImage,Image* destImage,Matrix algorithm){
     int row,pix,bit,span;
     span=srcImage->bpp*srcImage->bpp;
-    #pragma omp parallel for num_threads(thread_count) private(pix,bit)
+    //row does not need to be private
+    #pragma omp parallel for private(pix, bit)
     for (row=0;row<srcImage->height;row++){
         for (pix=0;pix<srcImage->width;pix++){
             for (bit=0;bit<srcImage->bpp;bit++){
@@ -114,7 +114,6 @@ int main(int argc,char** argv){
     destImage.height=srcImage.height;
     destImage.width=srcImage.width;
     destImage.data=malloc(sizeof(uint8_t)*destImage.width*destImage.bpp*destImage.height);
-    
     convolute(&srcImage,&destImage,algorithms[type]);
     stbi_write_png("output.png",destImage.width,destImage.height,destImage.bpp,destImage.data,destImage.bpp*destImage.width);
     stbi_image_free(srcImage.data);
